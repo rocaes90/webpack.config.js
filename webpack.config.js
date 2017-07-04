f1f577ad52ef550b649bd0a9e0a0e4020a439463
Roiner Camacho Esquivel <rocaes90@gmail.com>
2017-07-04 11:59:21 -0600

    Update webpack.config.js
    updating document

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  context: __dirname,
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/scripts.js",
-  output: {
-    path: __dirname + "/js",
-    filename: "scripts.min.js"
-  },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
-};
+    entry: './app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            {test: /\.(js)$/, use: 'babel-loader'},
+            {test: /\.css$/, use: [ 'style-loader', 'css-loader' ]},
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'app/index.html'
+        })
+    ]
+}
